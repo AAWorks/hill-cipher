bc93ab5fd4c1dc176704df14f09c9114b0bb6ef0
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-05-17 15:36:11 -0400

    added the to encrypt text to matrix part

--- cipher/basic-hill.py
+++ cipher/basic-hill.py
@@ -1,3 +1,9 @@
 import numpy as np
 
 # Simple Hill Cipher Implementation
+
+def encrypt(toEncrypt: str, key: str):
+    encryptMatrixIn = ''
+    for char in toEncrypt:
+        encryptMatrixIn += char + ' ; '
+    toProcessMatrix = np.matrix(encryptMatrixIn)
