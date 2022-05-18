d9e5a2f059302c873987c951fc9adfc98edcb47a
Andy Lin <alin21@stuy.edu>
2022-05-18 14:23:07 -0400

    prevent bad keys from handling this whole thing

--- cipher/basic-hill.py
+++ cipher/basic-hill.py
@@ -3,6 +3,9 @@ import numpy as np
 # Simple Hill Cipher Implementation
 
 def encrypt(toEncrypt: str, key: str):
+    if len(key) != len(toEncrypt) ** 2:
+        print("Key length is not string to encrypt length squared!")
+        return
     encryptMatrixIn = []
     for char in toEncrypt:
         encryptMatrixIn.append([ord(char)])
