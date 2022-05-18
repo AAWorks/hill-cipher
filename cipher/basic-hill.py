e038494c1f749404a2d3fafa7151d79c77f217c6
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-05-18 10:57:04 -0400

    fixed issue where adding things into matrix didnt work (changed to the numpy array because matrix class is deprecated), basic driver code

--- cipher/basic-hill.py
+++ cipher/basic-hill.py
@@ -3,7 +3,12 @@ import numpy as np
 # Simple Hill Cipher Implementation
 
 def encrypt(toEncrypt: str, key: str):
-    encryptMatrixIn = ''
+    encryptMatrixIn = []
     for char in toEncrypt:
-        encryptMatrixIn += char + ' ; '
-    toProcessMatrix = np.matrix(encryptMatrixIn)
+        encryptMatrixIn.append([ord(char)])
+    print(encryptMatrixIn)
+    toProcessMatrix = np.array(encryptMatrixIn)
+    print(toProcessMatrix)
+
+if __name__ == "__main__":
+    encrypt("rick", "")
