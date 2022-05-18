c2a3ff3cffbb682f3528e7120b44773033ff9090
Andy Lin <alin21@stuy.edu>
2022-05-18 14:24:21 -0400

    catch case for empty to encrypt string

--- cipher/basic-hill.py
+++ cipher/basic-hill.py
@@ -6,6 +6,8 @@ def encrypt(toEncrypt: str, key: str):
     if len(key) != len(toEncrypt) ** 2:
         print("Key length is not string to encrypt length squared!")
         return
+    elif len(toEncrypt) == 0:
+        print("No key inputted")
     encryptMatrixIn = []
     for char in toEncrypt:
         encryptMatrixIn.append([ord(char)])
