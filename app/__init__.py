8a3c6da2e6f4febcef8e46906ee074e59826ee4a
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2022-05-20 17:20:30 -0400

    edits suppsoed to be for result page

--- app/__init__.py
+++ app/__init__.py
@@ -9,15 +9,16 @@ def landing_page():
 
 @app.route('/machine', methods=['GET', 'POST'])
 def machine():
-    if request.method == "GET":
-        return render_template("landing_page.html")
-    else:
-        text = request.form["text"]
-        field00 = request.form["field00"]
-        field01 = request.form["field01"]
-        field10 = request.form["field10"]
-        field11 = request.form["field11"]
-        return render_template("result_page.html")
+    return render_template("machine.html")
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    text = request.form["text"]
+    field00 = request.form["field00"]
+    field01 = request.form["field01"]
+    field10 = request.form["field10"]
+    field11 = request.form["field11"]
+    return render_template("result_page.html")
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
