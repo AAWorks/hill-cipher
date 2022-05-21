be286ddb4d87ffb68a236ccd5fa420580021b54e
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2022-05-21 15:57:39 -0400

    getting encrypt to work

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,43 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 
+with open("app/cipher.py", "rb") as source_file:
+    code = compile(source_file.read(), "app/cipher.py", "exec")
+exec(code)
+
 app = Flask(__name__)
 app.secret_key = 'physiscmakesmesad'
 
 @app.route('/', methods=['GET','POST'])
 def landing_page():
-    return render_template("landing_page.html")
+    try:
+        return render_template("landing_page.html")
+    except:
+        return render_template("error.html")
 
 @app.route('/machine', methods=['GET', 'POST'])
 def machine():
-    return render_template("machine.html")
+    try:
+        return render_template("machine.html")
+    except:
+        return render_template("error.html")
 
-@app.route('/result', methods=['GET', 'POST'])
-def result():
+@app.route('/encode', methods=['GET', 'POST'])
+def encode():
+    text = request.form["text"].upper()
+    key = request.form["key"].upper()
+    if text == "" or key == "" or len(key) != len(text) ** 2:
+        return redirect('/machine')
+    result = encrypt(text, key)
+    return render_template("result_page.html", result=result)
+
+@app.route('/decode', methods=['GET', 'POST'])
+def decode():
     text = request.form["text"]
-    field00 = request.form["field00"]
-    field01 = request.form["field01"]
-    field10 = request.form["field10"]
-    field11 = request.form["field11"]
-    return render_template("result_page.html")
+    key = request.form["key"]
+    if text == "" or key == "" or len(key) != len(text) ** 2:
+        return redirect('/machine')
+    result="nothing here yet"
+    return render_template("result_page.html", result=result)
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
