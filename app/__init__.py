4dd25e53e7a748f3b7e9f3d2e023281ad9eb88cc
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2022-05-22 14:57:42 -0400

    streamlining, and a bit of complication to add loading page for funsies

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-import numpy as np
 
 with open("app/cipher.py", "rb") as source_file:
     code = compile(source_file.read(), "app/cipher.py", "exec")
@@ -22,26 +21,34 @@ def machine():
     except:
         return render_template("error.html")
 
-@app.route('/encode', methods=['GET', 'POST'])
+@app.route('/loading')
+def load():
+    try:
+        return render_template('loading_page.html')
+    except:
+        return render_template('error.html')
+
+@app.route('/cipher', methods=['GET', 'POST'])
 def encode():
+    global result
     text = request.form["text"]
     matrix = [[request.form["00"], request.form["01"]], [request.form["10"], request.form["11"]]]
-    if "" in matrix or text == "":
+    if text == "":
         return redirect('/machine')
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
+            if not matrix[i][j]:
+                return redirect('/machine')
             matrix[i][j] = int(matrix[i][j])
-    result = encrypt(text, matrix)
-    return render_template("result_page.html", result=result)
+    result = encrypt(text, matrix) if request.form["cipher"] == "Encode" else decrypt(text, matrix)
+    return redirect("/loading")
 
-@app.route('/decode', methods=['GET', 'POST'])
-def decode():
-    text = request.form["text"]
-    key = request.form["key"]
-    if text == "" or key == "" or len(key) != len(text) ** 2:
-        return redirect('/machine')
-    result="nothing here yet"
-    return render_template("result_page.html", result=result)
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    try:
+        return render_template('result_page.html', result=result)
+    except:
+        return render_template('error.html')
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
