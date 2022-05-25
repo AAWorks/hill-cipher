695a5399274e04a4f685b7352f10cafccc506e05
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2022-05-25 09:05:58 -0400

    yessir

--- app/cipher.py
+++ app/cipher.py
@@ -39,7 +39,7 @@ def encrypt(inputstr: str, keyMatrixIn: list, decoding: bool):
     if resultlist:
         ans += resultlist.pop(0)
     if decoding and ans[-1] == 'Z':
-        ans = ans[:-1] + " or " + ans
+        ans = ans[:-1]
     return ans
 
 def matInvMod (vmnp, mod):
